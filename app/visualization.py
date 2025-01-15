de01f5d79a8ebc81497b3c8235f041941d3108f3
Abhilasha <singhabhilasha4042@gmail.com>
2025-01-15 21:24:57 +0530

    ads embedding visualization

--- app/visualization.py
+++ app/visualization.py
@@ -138,7 +138,7 @@ if __name__ == "__main__":
         embedding_column='embedding',
         label_column='name',  # Optional
         reduction_method='tsne',   # or 'tsne'
-        n_components=3          # or 3 for 3D visualization
+        n_components=2          # or 3 for 3D visualization
     )
     
     # Display the plot
